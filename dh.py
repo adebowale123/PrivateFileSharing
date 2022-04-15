7ccf4c7039d4b6028dd3ffa4be9ae06446cae4aa
adebowale123 <[bowaleariyo@outlook.com]>
2022-04-15 16:05:26 +0100

    [updated comment dh.}

--- dh.py
+++ dh.py
@@ -1,27 +1,24 @@
+# use the class for individual object with the same behaviour 
 class DH_Endpoint(object):
+    # Calling the Initialization constructor as object is created from cthe class
+    # call self for easy accessing of instances created within class
     def __init__(self, public_key1, public_key2, private_key):
         self.public_key1 = public_key1
         self.public_key2 = public_key2
         self.private_key = private_key
         self.full_key = None
-        
+    # def to define the function created to generate Partial Key
     def generate_partial_key(self):
         partial_key = self.public_key1**self.private_key
         partial_key = partial_key%self.public_key2
         return partial_key
     
+    # def to define the function created to generate Full Key
     def generate_full_key(self, partial_key_r):
         full_key = partial_key_r**self.private_key
         full_key = full_key%self.public_key2
         self.full_key = full_key
         return full_key
-    
-    def encrypt_message(self, message):
-        encrypted_message = ""
-        key = self.full_key
-        for c in message:
-            encrypted_message += chr(ord(c)+key)
-        return encrypted_message
     
     
 u1_public=1257
