abe5aa9c8b5e7fec4e0a569823a099d3ab9d50aa
adebowale123 <[bowaleariyo@outlook.com]>
2022-04-15 16:17:29 +0100

    [updated c. steg}

--- stegano.py
+++ stegano.py
@@ -3,16 +3,16 @@ from PIL import Image
 import stepic
 
  #Open Image or file in which you want to hide your data
-im = Image.open('catoon.png')
+img = Image.open('catoon.png')
  #Encode some text into your Image file and save it in another file
-im1 = stepic.encode(im, b'Hello Python')
-im1.save('catoon.png', 'PNG')
+img1 = stepic.encode(img, b'Hello Python')
+img1.save('catoon.png', 'PNG')
 
  #Now is the time to check both images and see if there is any visible changes
-im1 = Image.open('catoon.png')
-im1.show()
+img1 = Image.open('catoon.png')
+img1.show()
 
 #Decode the image so as to extract the hidden data from the image
-im2 = Image.open('catoon.png')
-stegoImage = stepic.decode(im2)
+img2 = Image.open('catoon.png')
+stegoImage = stepic.decode(img2)
 stegoImage
